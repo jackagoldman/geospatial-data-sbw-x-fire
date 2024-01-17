bb38e17f6c625e91b513d2af8ae35a959b779e33
jackagoldman <jandrewgoldman@gmail.com>
2024-01-17 15:09:55 -0500

    add jpbw script

--- code/data_processing/jpbw.py
+++ code/data_processing/jpbw.py
@@ -33,12 +33,43 @@ for index, fire in fires.iterrows(): #for each row in fires
     print(jpbw)
     for index2, bw in jpbw.iterrows(): # for each row in jpbw
         if fire['geometry'].intersects(bw['geometry']):
-          data_defoliated.append({'geometry': fire['geometry'].intersection(bw['geometry']), 'defol_year': bw['Year'], 'fire_year': fire['Fire_Year']}) # can i add a column that calculates the difference between bw year and fire year?
-          data_non_defoliated.append({'geometry': fire['geometry'].difference(bw['geometry']), 'defol_year': bw['Year'], 'fire_year': fire['Fire_Year']})
+          data_defoliated.append({'geometry': fire['geometry'].intersection(bw['geometry']), 'defol_year': bw['Year'], 'fire_year': fire['Fire_Year'], 'fire_name': fire['Fire_ID']}) # can i add a column that calculates the difference between bw year and fire year?
+          data_non_defoliated.append({'geometry': fire['geometry'].difference(bw['geometry']), 'defol_year': bw['Year'], 'fire_year': fire['Fire_Year'], 'fire_name': fire['Fire_ID']})
 
-df = gp.GeoDataFrame(data_defoliated,columns=['geometry', 'defol_year', 'Fire_Year'])
+defol_fires = gp.GeoDataFrame(data_defoliated,columns=['geometry', 'defol_year', 'fire_year', 'fire_name']).to_crs(crs=3857)
+non_defol_fires = gp.GeoDataFrame(data_non_defoliated,columns=['geometry', 'defol_year', 'fire_year', 'fire_name']).to_crs(crs=3857)
 
-print(df)
+# make sure we take the greatest defoliated area so the greatest perimeter
+# for every fire, calculate the area of the perimeter
+# function for getting fire area
+
+def fire_area(x):
+    """fire area function returns the area of a fire 
+       as a new column in hectares (fire_area_ha)
+    """
+    for index, row in x.iterrows():
+        x['fire_area_ha']= x['geometry'].area / 1000
+        return x
+
+defol_fires.apply(fire_area, axis = 1)
+non_defol_fires.apply(fire_area, axis = 1)
+
+# loop through fires and return the largest area
+fire = defol_fires[defol_fires['fire_name']]
+data_defoliated2 = []
+
+for fire_name, fire in defol_fires.iteritems():
+    maxfire = fire.loc[fire['area'] == fire['area'].max()]
+    data_defoliated2.append({'geometry': maxfire['geometry'], 'defol_year': maxfire['defol_year'], 'fire_year': maxfire['fire_year'], 'fire_name': maxfire['fire_name'] })
+
+
+defol_fires2 = gp.GeoDataFrame(data_defoliated2,columns=['geometry', 'defol_year', 'fire_year', 'fire_name'])
+print(defol_fires2)
+
+
+
+
+
 
 
 
