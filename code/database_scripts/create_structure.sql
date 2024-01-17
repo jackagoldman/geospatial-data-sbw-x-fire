97e2301b77d862876a276c1aab6339bd59331373
jackagoldman <jandrewgoldman@gmail.com>
2024-01-16 22:18:49 -0500

    add sql script

--- code/database_scripts/create_structure.sql
+++ code/database_scripts/create_structure.sql
@@ -7,7 +7,6 @@ CREATE TABLE fire_key(
     id integer PRIMARY KEY,
     fire_name character varying,
     year bigint,
-    defol boolean, -- Use 1 or 0 to specify whether it is the defolaited or non defoliated perimeter... use as a key?
 )
 
 -- create insect_disturbance table
@@ -15,74 +14,43 @@ CREATE DATABASE insect_disturbance(
     id integer PRIMARY KEY,
     fire_name character varying,
     tsd integer,
-    years_defoliated integer
+    years_defoliated integer,
+    defol boolean, -- Use 1 or 0 to specify whether it is the defolaited or non defoliated perimeter... use as a key?
+
 )
 
 -- create nbr recovery table for defoliated fires 
-CREATE TABLE defol_nbr_recovery(
+CREATE TABLE nbr_recovery(
     id integer PRIMARY KEY,
     fire_name character varying, 
-    defol_nbr_1 bigint,
-    defol_nbr_2 bigint,
-    defol_nbr_3 bigint,
-    defol_nbr_4 bigint,
-    defol_nbr_5 bigint, 
-    defol_nbr_6 bigint, 
-    defol_nbr_7 bigint, 
-    defol_nbr_8 bigint, 
-    defol_nbr_9 bigint, 
-    defol_nbr_10 bigint
+    nbr_1 bigint,
+    nbr_2 bigint,
+    nbr_3 bigint,
+    nbr_4 bigint,
+    nbr_5 bigint, 
+    nbr_6 bigint, 
+    nbr_7 bigint, 
+    nbr_8 bigint, 
+    nbr_9 bigint, 
+    nbr_10 bigint
 )
 
--- create nbr recovery table for non-defoliated fires 
-CREATE TABLE nondefol_nbr_recovery(
-    id integer PRIMARY KEY,
-    fire_name character varying, 
-    nondefol_nbr_1 bigint,
-    nondefol_nbr_2 bigint,
-    nondefol_nbr_3 bigint,
-    nondefol_nbr_4 bigint,
-    nondefol_nbr_5 bigint, 
-    nondefol_nbr_6 bigint, 
-    nondefol_nbr_7 bigint, 
-    nondefol_nbr_8 bigint, 
-    nondefol_nbr_9 bigint, 
-    nondefol_nbr_10 bigint
-)
 -- create burn severity table for defol fires 
-CREATE TABLE defol_burn_severity(
+CREATE TABLE burn_severity(
     id integer PRIMARY KEY,
     fire_name character varying,
-    defol_rbr_median bigint,
-    defol_rbr_extreme bigint,
-    defol_rbr_cv bigint
+    rbr_median bigint,
+    rbr_extreme bigint,
+    rbr_cv bigint
 )
 
--- create burn severity table for nondefol fires 
-CREATE TABLE nondefol_burn_severity(
+-- forest cover 
+CREATE TABLE forest_cover(
     id integer PRIMARY KEY,
     fire_name character varying,
-    nondefol_rbr_median bigint,
-    nondefol_rbr_extreme bigint,
-    nondefol_rbr_cv bigint
-)
-
--- forest cover for defoliated fires
-CREATE TABLE defol_forest_cover(
-    id integer PRIMARY KEY,
-    fire_name character varying,
-    defol_biomass bigint,
-    defol_stand_age bigint,
-    defol_canopy_closure bigint
-)
-
--- forest cover for nondefoliated fires
-CREATE TABLE defol_forest_cover(
-    id integer PRIMARY KEY,
-    fire_name character varying,
-    nondefol_biomass bigint,
-    nondefol_stand_age bigint,
-    nondefol_canopy_closure bigint
+    biomass bigint,
+    stand_age bigint,
+    canopy_closure bigint
 )
 
 -- create weather data (fwi-90th-fire-duration.csv)
@@ -97,38 +65,38 @@ CREATE TABLE fwi_duration(
     dmc_duration bigint
 )
 
--- create table for defoliated fire characteristics
-CREATE TABLE defol_fire_characteristics(
+-- create table for  fire characteristics
+CREATE TABLE fire_characteristics(
     id integer PRIMARY KEY,
     fire_name character varying,
-    defol_perimeter bigint,
-    defol_area bigint,
-    defol_shapeindex bigint,
-    defol_burnday integer,
-)
-
--- create table for nondefoliated fire characteristics
-CREATE TABLE nondefol_fire_characteristics(
-    id integer PRIMARY KEY,
-    fire_name character varying,
-    nondefol_perimeter bigint,
-    nondefol_area bigint,
-    nondefol_shapeindex bigint,
-    nondefol_burnday integer,
+    burn_day integer
 )
 
 -- create snow cover table for defoliated fires
 CREATE TABLE snow_cover(
     id integer PRIMARY KEY,
     fire_name character varying,
-    defol_sfd integer,
-    defol_sf
+    sfd integer,
+    swe integer
 )
 
--- create spatial data table for defoliated
-CREATE TABLE defol_spatial(
+-- create spatial information tablr
+CREATE TABLE defol_spatial_information(
     id ineger PRIMARY KEY,
-    fire_name character varying,
-    defol_fire_geometry polygon,
-    defol_fire_centroid point
+    fire_geometry polygon,
+    fire_centroid point,
+    fire_perimeter_m bigint,
+    fire_area_ha bigint,
+    fire_area_m bigint,
+    fire_shape_index bigint,
+)
+
+-- create harvesting table
+
+CREATE TABLE harvest(
+
+
+
+
+    
 )
